aaade56619f4900cea99cc05ee3b7b92f585544d
Arek P <sandmann@o2.pl>
2021-07-25 23:37:57 +0200

    Reinventing Models

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -5,8 +5,11 @@ from .models import Task, Category
 # admin.site.register(Task)
 admin.site.register(Category)
 
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'author', 'name', 'category')
-    list_filter = ('id', 'author', 'category')
-    search_fields = ('name', 'description', 'details')
+    list_display = ('name', 'date_due', 'priority', 'category', 'date_created', 'author')
+    list_filter = ('author', 'priority', 'date_due')
+    search_fields = ('name', 'description', 'details', 'category')
+    date_hierarchy = 'date_created'
+    ordering = ('date_due', 'priority', 'category')
--- todo_app/models.py
+++ todo_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-from datetime import datetime, date
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -22,20 +22,22 @@ class Task(models.Model):
         ('High', 'High'),
         ('Highest', 'Highest'),
     )
-    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='tasks')
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=100, null=True, blank=True)
     details = models.TextField(null=True, blank=True)
     category = models.CharField(max_length=50, null=True, blank=True)
     priority = models.CharField(max_length=255, null=True, choices=PRIORITY)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    date_created = models.DateTimeField(default=timezone.now)
     date_due = models.DateField(null=True, blank=True)
     # reminder = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ('date_due',)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        # return reverse('task_details', kwargs={'pk': self.pk})
         return reverse('task_list')
 
